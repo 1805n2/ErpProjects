b0897aba4a1d2de65c53ab8db9c3ce008fa83dd4
wx <166@qq.com>
2018-06-13 18:35:45 +0800

    王希的第二次提交

--- DAL/cgManager/cgqgservice.cs
+++ DAL/cgManager/cgqgservice.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace DAL.cgManager
 {
-    class cgqgservice
+    public class cgqgservice
     {
+
     }
 }
